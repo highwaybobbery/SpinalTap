f84a9da7b8cfbeb3ed723692e14b4b63a5b2d438
Alex Berry <highwaybobbery@gmail.com>
2012-06-01 11:09:03 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpinalTap::Application.config.secret_token = '8e57402bdf3cfc71b3e5b89457244e45c061341d8454ecf3fb9251c8872fad6f9078001ed2b2d2ba9e9b886236cd82061aa76f512af48c4e8b1b8f6abf642ec1'

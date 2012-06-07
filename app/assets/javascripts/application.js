b3e3be976bf84e8a837b0c5a6b8ab8aaae9d5eec
Alex Berry <highwaybobbery@gmail.com>
2012-06-06 22:06:42 -0400

    wip

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,13 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require underscore
+//= require backbone
+//= require backbone-support
+//= require spinal-tap
+//= require_tree ../templates
+//= require_tree ./models
+//= require_tree ./collections
+//= require_tree ./views
+//= require_tree ./routers
 //= require_tree .
